2602ab018b00151e974d3ad97eeffba4ecaefef5
BravoCoders <bravocodersunited@gmail.com>
2025-08-05 17:39:03 +0530

    Delete models/0tps.js

--- models/0tps.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const otpSchema = new mongoose.Schema({
-  message: String,
-  timestamp: Number
-});
-
-module.exports = mongoose.models.Otp || mongoose.model('Otp', otpSchema);
